096d18a522b6a946ecf509fc925059c03fbd8d9e
NikolaPozega <nikola@vila-stanisic.hr>
2026-06-10 17:58:55 +0200

    Add @babel/plugin-transform-classes (loose) — fix ES6 class hermesc error

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -3,9 +3,10 @@ module.exports = function (api) {
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
     plugins: [
-      "@babel/plugin-transform-class-properties",
-      "@babel/plugin-transform-private-methods",
-      "@babel/plugin-transform-private-property-in-object",
+      ["@babel/plugin-transform-class-properties", { loose: true }],
+      ["@babel/plugin-transform-private-methods", { loose: true }],
+      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+      ["@babel/plugin-transform-classes", { loose: true }],
     ],
   };
 };
