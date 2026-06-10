676fa24a75e9274e393f4edfcd1bffc4a112f3b8
NikolaPozega <nikola@vila-stanisic.hr>
2026-06-10 18:38:42 +0200

    Fix Babel ordering: TypeScript transform only for .ts/.tsx via overrides

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -1,13 +1,27 @@
 module.exports = function (api) {
   api.cache(true);
+
+  const classCompatPlugins = [
+    ["@babel/plugin-transform-class-properties", { loose: true }],
+    ["@babel/plugin-transform-private-methods", { loose: true }],
+    ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+    ["@babel/plugin-transform-classes", { loose: true }],
+  ];
+
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
-    plugins: [
-      ["@babel/plugin-transform-typescript", { allowDeclareFields: true }],
-      ["@babel/plugin-transform-class-properties", { loose: true }],
-      ["@babel/plugin-transform-private-methods", { loose: true }],
-      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
-      ["@babel/plugin-transform-classes", { loose: true }],
+    overrides: [
+      {
+        test: /\.[cm]?tsx?$/,
+        plugins: [
+          ["@babel/plugin-transform-typescript", { allowDeclareFields: true }],
+          ...classCompatPlugins,
+        ],
+      },
+      {
+        test: /\.[cm]?jsx?$/,
+        plugins: classCompatPlugins,
+      },
     ],
   };
 };
